6378163c8fe2504e322e1266012ae1f2f74f1ad4
Ubuntu <ubuntu@ip-172-31-88-97.ec2.internal>
2023-05-29 12:24:29 +0000

    template

---
+++ devsecondtemp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+GIT=git@github.com:syamu99/Devops2.git
+REPO=Devops2.git
+WEBSITE=https://www.tooplate.com/zip-templates/2103_central.zip
+PKG=apt
+FILE=2103_central
+sudo $PKG update
+sudo $PKG install wget git unzip figlet -y
+git clone $GIT
+mkdir -p secondtemp
+cd secondtemp
+sudo wget $WEBSITE
+sudo unzip -o $FILE.zip
+sudo rm -rf ~/$REPO/*
+sudo cp -r $FILE/* ~/$REPO/
+cd ..
+sudo rm -rf secondtemp
+cd ~/$REPO
+git add .
+git status
+sleep 2
+git commit -m "$(date)"
+git push -f
+figlet done
